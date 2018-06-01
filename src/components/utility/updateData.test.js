f82590c09ee870022b3d325ae420faf7b5e59252
Helen Zhou <helen.zhou6@gmail.com>
2018-06-01 10:07:55 +0100

    Add extra test to ensure the data set has been altered after updateData()

--- src/components/utility/updateData.test.js
+++ src/components/utility/updateData.test.js
@@ -2,5 +2,6 @@ import updateData from "./updateData";
 import { oldData, updatedEntry, expected } from "./dummyData.js";
 test("Test updateData function", () => {
 	expect(updateData(oldData, updatedEntry)).toEqual(expected);
+	expect(oldData).not.toEqual(expected);
 });
 
