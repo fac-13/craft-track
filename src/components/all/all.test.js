0b1ed0410bd197f425ef1cb7120f3e8dd244a15f
tdoran <tomgdoran@gmail.com>
2018-06-04 17:36:41 +0100

    remove consolelog

--- src/components/all/all.test.js
+++ src/components/all/all.test.js
@@ -5,8 +5,6 @@ import { oldData } from "../utility/dummyData";
 
 test("Test Landing component", () => {
 	const { container } = render(<All crafts={oldData} />);
-	console.log(prettyDOM(container));
-	console.log(oldData[0].id);
 	expect(prettyDOM(container).includes("icon__shoe")).toBeTruthy();
 
 });
