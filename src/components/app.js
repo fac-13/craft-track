ba324a57d77f4504e9bb1ccaf0abdbad1594a629
Helen Zhou <helen.zhou6@gmail.com>
2018-06-04 16:06:08 +0100

    Add dynamic page rendering, linking to arrow icon on landing page relate #7

--- src/components/app.js
+++ src/components/app.js
@@ -5,12 +5,28 @@ import Frame from "./styled/frame/frame";
 import Landing from "./landing/landing";
 
 export default class App extends React.Component {
-	state = {};
+	state = {
+		pageView: "landing"
+	};
+
+	changePage = (e, page) => {
+		e.preventDefault();
+		this.setState(() => {
+			return { pageView: page };
+		});
+	}
+
 	render() {
+		let page = <Landing changePage={this.changePage} />;
+		if (this.state.pageView == "viewall") {
+			page = <h1>All products view</h1>;
+		} else if (this.state.pageView == "form") {
+			page = <h1>Form</h1>;
+		}
 		return (
 			<React.Fragment>
 				<Frame position="top" />
-				<Landing />
+				{page}
 				<Frame position="bottom" />
 			</React.Fragment>
 		);
